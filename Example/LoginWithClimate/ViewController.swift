f961a6cf14e8d67cc5214f40f61b36ad21206219
Tommy Rogers <tommy.rogers@climate.com>
2016-01-21 13:04:19 -0800

    Add login button to Example app

--- Example/LoginWithClimate/ViewController.swift
+++ Example/LoginWithClimate/ViewController.swift
@@ -7,12 +7,22 @@
 //
 
 import UIKit
+import LoginWithClimate
 
 class ViewController: UIViewController {
 
+    @IBOutlet var contentView: UIView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        let loginView = LoginWithClimateButton()
+        contentView.addSubview(loginView)
+
+
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":loginView]))
+        contentView.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[view]-0-|", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["view":loginView]))
     }
 
     override func didReceiveMemoryWarning() {
