95296ef6a99928cfa4f000aea99cd35af397b3f6
Tommy Rogers <tommy.rogers@climate.com>
2016-01-25 11:00:45 -0800

    Intercept redirects from webview

--- Pod/Classes/ClimateWebViewController.swift
+++ Pod/Classes/ClimateWebViewController.swift
@@ -9,24 +9,26 @@
 import Foundation
 import UIKit
 
-class ClimateWebViewController: UIViewController {
+class ClimateWebViewController: UIViewController, UIWebViewDelegate {
 
     @IBOutlet var webView: UIWebView!
 
-    let loginPageQueryParams = [ "page": "oidcauthn",
+    let loginPageQueryParams: [NSURLQueryItem] = [ "page": "oidcauthn",
                                  "client_id": "authorize",
                                  "response_type": "code",
-                                 "redirect_uri": "https://woo/ha",
+                                 "redirect_uri": "done:",
                                  "scope": "openid user"
-                               ]
+                               ].map({(k, v) in NSURLQueryItem(name: k, value: v) })
 
     override func viewDidLoad() {
 
         let l = NSURLComponents(string: "https://qa1.climate.com/static/auth-pages/index.html")!
-        l.queryItems = loginPageQueryParams.map({(k, v) in NSURLQueryItem(name: k, value: v) })
+        l.queryItems = loginPageQueryParams
         let loginPageURL = l.URL!
 
         print(loginPageURL)
+
+        webView.delegate = self
 
         webView.loadRequest(NSURLRequest(URL: loginPageURL))
         webView.scrollView.scrollEnabled = false
@@ -34,6 +36,42 @@ class ClimateWebViewController: UIViewController {
 
     @IBAction func cancel(sender: AnyObject) {
         self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+
+    func webView(webView: UIWebView, didFailLoadWithError error: NSError?) {
+        if (error?.code == NSURLErrorCancelled || error?.code == 102 || error?.code == 101) {
+            return
+        }
+
+        print("Webview failed to load with error: \(error)")
+
+    }
+
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        print(request)
+        print(request.URL?.host)
+
+        // TODO can this all happen on this thread before returning?
+        if (request.URL?.scheme == "done") {
+            self.dismissViewControllerAnimated(true, completion: nil)
+            if let queryItems = NSURLComponents(URL: request.URL!, resolvingAgainstBaseURL: false)?.queryItems {
+                let code = queryItems.filter({(item) -> Bool in item.name == "code"}).first
+                print("Authorization code is: \(code)")
+                print("")
+            }
+
+            request.URL?.query
+        }
+
+        return true
     }
 
 }
